2bb0923beb938d3e639d69786ae8796d60d5753f
Rafael Colin <faelnek@gmail.com>
2023-05-29 16:40:03 -0300

    pt2 of the project

--- src/main/java/com/colin/api/controller/GreetingController.java
+++ src/main/java/com/colin/api/controller/GreetingController.java
@@ -1,9 +1,7 @@
 package com.colin.api.controller;
 
 import com.colin.api.entities.Greeting;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -17,5 +15,12 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
     }
 }
