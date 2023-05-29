ad9f72c7a8e50446dee398b4fb935adc6fd63a3c
Rafael Colin <faelnek@gmail.com>
2023-05-29 14:00:05 -0300

    Starting project

---
+++ src/test/java/com/colin/api/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
