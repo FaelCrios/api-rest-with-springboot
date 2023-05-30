39b8fd4fa4e4788fab9ad3bc6d30f2a8cb5b4dae
Rafael Colin <faelnek@gmail.com>
2023-05-30 16:05:34 -0300

    Add JPA repository and refactoring all the crud system

--- src/main/java/com/colin/api/controller/PersonController.java
+++ src/main/java/com/colin/api/controller/PersonController.java
@@ -5,12 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.colin.api.entities.Person;
 import com.colin.api.services.PersonServices;
 
@@ -23,33 +19,34 @@ public class PersonController {
     //private PersonServices service = new PersonServices();
 
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") String id) throws Exception{
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") Long id) throws Exception{
 				return service.findById(id);
     	
     }
     
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll(){
 				return service.findAll();
     	
     }
     
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person createPerson(@RequestBody Person person){
 				return service.createPerson(person);
     	
     }
     
-    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person updatePerson(@RequestBody Person person){
     	return service.updatePerson(person);
     	
     }
     
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void deleteById(@PathVariable(value = "id") String id) throws Exception{
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable(value = "id") Long id) throws Exception{
 				service.deletePerson(id);
+				return ResponseEntity.noContent().build();
     	
     }
     
--- src/main/java/com/colin/api/services/PersonServices.java
+++ src/main/java/com/colin/api/services/PersonServices.java
@@ -1,64 +1,59 @@
 package com.colin.api.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.colin.api.entities.Person;
+import com.colin.api.exceptions.ResourceNotFoundException;
+import com.colin.api.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
-    private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	@Autowired
+	PersonRepository repository;
 	
 	public List<Person> findAll() {
 		logger.info("Finding All people!");
-		List<Person> persons = new ArrayList<Person>();
-		for(int i = 0; i < 8; i++) {
-			Person person = mockPerson(i);
-			persons.add(person);
-		}
-		return persons ;
+		return repository.findAll();
 	
 	}	
 
-	public Person findById(String id) {
+	public Person findById(Long id) {
 		logger.info("Finding one person!");
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Rafa");
-		person.setLastName("Colin");
-		person.setAddress("Bauru");
-		person.setGender("male");
-		return person;
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
 		
 	}
 	
-	private Person mockPerson(int i) {
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Person name: "+ i);
-		person.setLastName("Last name: "+ i);
-		person.setAddress("Some address in Brazil: "+ i);
-		person.setGender("Gender: "+ i);
-		return person;
-	}
-
 	public Person createPerson(Person person) {
 		logger.info("Creating one person!");
-		return person;
+		return repository.save(person);
 	}
 	
 	public Person updatePerson(Person person) {
 		logger.info("Updating one person!");
-		return person;
+		
+		Person entity = repository.findById(person.getId())
+		.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(person);
 	}
 	
-	public void deletePerson(String id) {
+	public void deletePerson(Long id) {
 		logger.info("Deleting one person!");
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		repository.delete(entity);
+				
 	}
 }
